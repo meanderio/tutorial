1774425153338bf82af780a994e4b26eebf3ef77
meanderio <meanderings.io>
2023-08-06 15:22:27 -0500

    rss feed created

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Meanderings.io | Blog',
+    description: 'My journey learning code and data science',
+    site: 'https://meanderings.io',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
