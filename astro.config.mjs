19354714b6e928c1f27018f124745eb29bef8281
meanderio <acahn@meanderings.io>
2023-08-11 20:47:58 -0500

    codeblock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), mdx(), tailwind()]
 });
